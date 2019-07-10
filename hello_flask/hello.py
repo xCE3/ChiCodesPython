ef47b4ca3f56c2bfe219f0e9d1bf7e1020e3f998
Charles <41131389+xCE3@users.noreply.github.com>
2019-07-10 13:41:08 -0500

    Add files via upload

---
+++ hello_flask/hello.py
@@ -0,0 +1,24 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'
+    
+@app.route('/dojo')          # The "@" decorator associates this route with the function immediately following
+def hello_Dojo():
+    return 'Dojo!'
+
+@app.route('/<name>')          # The "@" decorator associates this route with the function immediately following
+def hello_person(name):
+    return f'Hi {name}!'
+
+@app.route('/<name>')          # The "@" decorator associates this route with the function immediately following
+def hello_person(name):
+    return f'Hi {name}!'
+
+@app.route('/repeat/<qty>/<name>')
+def repeat(qty, name):
+    return name*int(qty)
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
