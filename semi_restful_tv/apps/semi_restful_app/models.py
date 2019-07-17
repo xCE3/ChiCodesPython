a95f2f3c1004815fa9011e48e62507d4c3d1fdb7
Charles <41131389+xCE3@users.noreply.github.com>
2019-07-17 10:42:56 -0500

    Add files via upload
    WITH VALIDATIONS

--- semi_restful_tv/apps/semi_restful_app/models.py
+++ semi_restful_tv/apps/semi_restful_app/models.py
@@ -1,4 +1,19 @@
+
 from django.db import models
+
+class ShowManager(models.Manager):
+
+    def validate(request, postdata):
+        errors = {}
+        print (" validate ")
+        print (postdata)
+        if (not postdata['title'].isalpha() or len(postdata['title']) < 2):
+            errors['title'] = "Error: Title should be at least 2 characters and cannot be empty"
+        if (not postdata['network'].isalpha() or len(postdata['network']) < 3):
+            errors['network'] = "Error: Network should be at least 3 characters and cannot be empty"
+        if (not postdata['desc'].isalpha() or len(postdata['desc']) < 10):
+            errors['desc'] = "Error: Description should be at least 10 characters and cannot be empty"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=60)
@@ -6,3 +21,7 @@ class Show(models.Model):
     desc = models.CharField(max_length=60)
     releasedate = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+
+    def __unicode__(self):
+        return " id: " + str(self.id) + " title: " + self.title + " network: " + self.network + " network: " + self.network
--- semi_restful_tv/apps/semi_restful_app/views.py
+++ semi_restful_tv/apps/semi_restful_app/views.py
@@ -20,11 +20,20 @@ def edit(request, id):
 
 def show(request, id):
     context = {
-        'show' : Show.objects.get(id=id)
+    'show' : Show.objects.get(id=id)
     }
     return render(request, "semi_restful_app/show.html", context)
 
 def create(request):
+    #validate data
+    errors = Show.objects.validate(request.POST)
+    if (errors):
+        print ("==== errror ")
+        for error in errors:
+            messages.error(request, errors[error])
+        print (messages)
+        return redirect("/new")
+    print ("===== no errors ===")
     Show.objects.create(title=request.POST['title'], network=request.POST['network'], releasedate=request.POST['releasedate'])
     return redirect("/shows")
 
@@ -33,6 +42,16 @@ def destroy(request, id):
     return redirect("/shows")
 
 def update(request):
+    print ("------update")
+    # validate data
+    errors = Show.objects.validate(request.POST)
+    if (errors):
+        print ("==== errror ")
+        for error in errors:
+            messages.error(request, errors[error])
+        print (messages)
+        reURL = "/shows/"+ str(request.POST['id']) + "/edit"
+        return redirect(reURL)
     u = Show.objects.get(id=request.POST['id'])
     u.title = request.POST['title']
     u.network = request.POST['network']
