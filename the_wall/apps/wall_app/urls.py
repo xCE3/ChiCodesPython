b80bb9ba224edbeae5b5cfb1bcc3d75800af1302
Charles <41131389+xCE3@users.noreply.github.com>
2019-07-22 11:36:02 -0500

    Add files via upload

---
+++ the_wall/apps/wall_app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^register$', views.register),
+    url(r'^login$', views.login),
+    url(r'^wall$', views.wall),
+    url(r'^post$', views.post),
+    url(r'^comment$', views.comment),
+    url(r'^logout$', views.logout),
+    url(r'^delmsg/(?P<id>\d+)$', views.delmsg),
+    url(r'^delcom/(?P<id>\d+)$', views.delcom)
+]
